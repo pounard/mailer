9ce99f320f61fb65ef43b245ee8ac54478dc4280
Pierre Rineau <pierre.rineau@makina-corpus.com>
2013-11-15 18:10:51 +0100

    Added sample config file

---
+++ etc/sample.config.php
@@ -0,0 +1,15 @@
+<?php
+return array(
+    'smtp' => array(
+        'secure' => true,
+        'host' => 'smtp.example.com',
+        'username' => 'someuser',
+        'password' => 'somepassword',
+    ),
+    'imap' => array(
+        'secure' => true,
+        'host' => 'imap.example.com',
+        'username' => 'someuser',
+        'password' => 'somepassword',
+    ),
+);
